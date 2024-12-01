464d8f6f4537aa6a70a6172b3f2f27b153323362
Sniped <colin@colinmcc.me>
2024-12-01 15:03:52 -0500

    initial commit (basic bun structure)

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify';
+import websocketPlugin from '@fastify/websocket';
+
+const fastify = Fastify({ logger: true });
+
+fastify.register(websocketPlugin);
+
+fastify.listen({ port: 3000 }, (err, address) => {
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+
+	fastify.log.info(`server listening on ${address}`);
+});
