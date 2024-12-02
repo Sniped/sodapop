e244bc1f0a04f58203c4050e8d5c0049c65cd69c
CaydenKeene <cayden.keenefl@gmail.com>
2024-12-02 01:32:37 -0500

    datamanager update

--- frontend/datamanager.h
+++ frontend/datamanager.h
@@ -1,27 +1,29 @@
+// datamanager.h
 #ifndef DATAMANAGER_H
 #define DATAMANAGER_H
 
 #include <QObject>
 #include <QWebSocket>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkRequest>
 
-class DataManager : public QObject {
+class DataManager : public QObject
+{
     Q_OBJECT
-    Q_PROPERTY(double currentValue READ currentValue NOTIFY currentValueChanged)
 
 public:
     explicit DataManager(QObject *parent = nullptr);
-    double currentValue() const { return m_currentValue; }
-
-signals:
-    void currentValueChanged();
+    void connectToServer();
 
 private slots:
     void onConnected();
-    void onMessageReceived(const QString &message);
+    void onDisconnected();
+    void onTextMessageReceived(const QString &message);
 
 private:
     QWebSocket m_webSocket;
-    double m_currentValue;
+    void parseDeviceData(const QJsonObject &data);
 };
 
 #endif // DATAMANAGER_H
